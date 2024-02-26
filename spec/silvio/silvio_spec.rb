49bb4aea590471f9286323a25f9dff7b4e802d26
Joost Van Averbeke <Joost.VanAverbeke@clinisys.com>
2024-02-26 08:36:34 +0100

    Added an excercise using RSpec and the Silvio class

--- spec/silvio/silvio_spec.rb
+++ spec/silvio/silvio_spec.rb
@@ -4,5 +4,12 @@ describe Silvio do
 	it 'rocks' do
 		silvio = Silvio.new
 		expect(silvio.start).to eq('rocks')
-	end
+  end
+
+  # Change the constructor of the Silvio class, so that it optionally takes the name of the user as an argument.
+  # If the user name argument is not specified it should take user name = 'silvio' as the default user name.
+  # Add an instance method 'like_to_work', to the silvio class that returns the following string:
+  # 'I, <user name> like to work with silvio'
+  #
+  # Use RSpec based test driven development to implement the above excercise.
 end
